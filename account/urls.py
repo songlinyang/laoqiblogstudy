8290819f0a567732eead44610d244ffe9755fd8d
slyang <yang_song_lin@163.com>
2019-11-06 19:05:50 +0800

    重置密码

--- account/urls.py
+++ account/urls.py
@@ -9,6 +9,10 @@ urlpatterns = [
     path('newlogin/',auth_views.LoginView.as_view(),name="user_login"),
     path('logout/',auth_views.LogoutView.as_view(),name="user_logout"),
     path('register/',views.register,name="register"),
-    path('password_change/',auth_views.PasswordChangeView.as_view(),name="password_change"),
-    path('password_change_done/',auth_views.PasswordChangeDoneView.as_view(),name="password_change_done"),
+    path('password_change/',auth_views.PasswordChangeView.as_view(template_name="account/password_change_form.html",success_url="/account/password_change_done/"),name="password_change"),
+    path('password_change_done/',auth_views.PasswordChangeDoneView.as_view(template_name="account/"),name="password_change_done"),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
